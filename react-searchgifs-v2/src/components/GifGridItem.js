2e221ef79b4c73d08c03df927be33e012f400493
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-08-27 03:12:52 -0300

    Update GifGridItem.js

--- react-searchgifs-v2/src/components/GifGridItem.js
+++ react-searchgifs-v2/src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ( {id, title, url} ) => {
     
 
     return (
-        <div className="card animate__rollIn">
+        <div className="card animate__animated animate__rollIn">
             <img src={url} alt={title} />
             <p>{title}</p>
         </div>
