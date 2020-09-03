bd34ae4c3458ba96c6dfa552e2f035b5680d76b5
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-03 18:00:35 -0300

    Update AddCategory.js

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -7,6 +7,7 @@ export const AddCategory = ( { setCategories } ) => {
     
     const handleInputChange = ( e ) =>{
         setInputValue( e.target.value );
+
     }
 
     const handleSubmit = ( e ) => {
