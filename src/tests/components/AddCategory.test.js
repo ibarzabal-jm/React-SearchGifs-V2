e391f6a5e2abc584f2f1af9c479abf30a6368f09
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-03 18:00:41 -0300

    Update AddCategory.test.js

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import '@testing-library/jest-dom'
 import { shallow } from 'enzyme'
 import { AddCategory } from '../../components/AddCategory'
 
 describe('Pruebas en <AddCAtegory />', () => {
     
     const setCategories = () =>{};
+    const wrapper = shallow( <AddCategory setCategories={setCategories} /> );
     
     test('debe de mostrarse correctamente', () => {
-        const wrapper = shallow( <AddCategory setCategories={setCategories} /> );
 
-        
+        expect( wrapper ).toMatchSnapshot();
     })
+
+    test('debe de cambiar la caja de texto ', () => {
+        
+        const input = wrapper.find('input');
+        const value ='Hola mundo'
+
+        input.simulate('change', { target : { value } } );
+    })
+    
     
 })
