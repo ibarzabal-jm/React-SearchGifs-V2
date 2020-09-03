e89f15e6f9d078c3719be4a8c6d2086c0065fcec
ibarzabal-jm <ibarzabalsallesjm@gmail.com>
2020-09-03 17:41:49 -0300

    Update getGifs.test.js

--- src/tests/components/getGifs.test.js
+++ src/tests/components/getGifs.test.js
@@ -9,4 +9,11 @@ describe('Pruebas con getGifs fetch', () => {
         expect( gifs.length ).toBe( 5 );
     })
     
+    test('debe de traer 5 elementos', async() => {
+        
+        const gifs = await getGifs('');
+
+        expect( gifs.length ).toBe( 0 );
+    })
+    
 })
